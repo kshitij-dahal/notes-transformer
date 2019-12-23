77990c346d739d282564a9db2086c556044937a0
kshitij314 <kshitijdahal314@gmail.com>
2019-12-22 20:48:00 -0500

    Created package for view classes (of MVC). Generated empty activity for the main select pdf screen.

---
+++ app/src/main/java/com/NotesTransformer/View/MainSelectPDFView.java
@@ -0,0 +1,14 @@
+package com.NotesTransformer.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.notestransformer.R;
+
+public class MainSelectPDFView extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main_select_pdf);
+  }
+}
