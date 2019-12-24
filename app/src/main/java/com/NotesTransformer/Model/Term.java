52f838118a850735737f8dbac477fce57aebeb7f
kshitij314 <kshitijdahal314@gmail.com>
2019-12-24 08:54:23 -0500

    Implemented Term class with a String field containing the term.

---
+++ app/src/main/java/com/NotesTransformer/Model/Term.java
@@ -0,0 +1,19 @@
+package com.NotesTransformer.Model;
+
+public class Term {
+
+  private String text;
+
+  public Term(String text) {
+    this.text = text;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+}
